902c6ef123e985d21ff2d1b26f40fa6a48603ef0
rojolang <rjjm94@gmail.com>
2024-12-22 02:23:07 -0600

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['images.unsplash.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com', 'reliablepph.com'],
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
   },
   webpack: (config) => {
     config.resolve.fallback = {
